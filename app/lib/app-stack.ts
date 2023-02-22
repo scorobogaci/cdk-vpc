aad488726531862c91bd9829341759ed4a7135b7
ion <scorobogaciion@gmail.com>
2023-02-22 21:23:00 +0200

    Added VPC definition

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Construct} from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class AppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        new ec2.Vpc(this, 'SampleVPC', {
+            vpcName: 'SampleVpc',
+            natGateways: 1,
+            maxAzs: 3,
+            subnetConfiguration: [
+                {
+                    name: 'private-subnet-1',
+                    subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                    cidrMask: 24,
+                },
+                {
+                    name: 'public-subnet-1',
+                    subnetType: ec2.SubnetType.PUBLIC,
+                    cidrMask: 24,
+                },
+                {
+                    name: 'isolated-subnet-1',
+                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                    cidrMask: 28,
+                },
+            ],
+        });
+    }
 }
